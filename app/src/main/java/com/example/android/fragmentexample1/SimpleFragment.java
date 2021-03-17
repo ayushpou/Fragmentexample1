554d03fd49880eb0cba4527f3d08ed03fe7b7587
c7202791 <ayuxpoudel4@gmail.com>
2021-03-17 10:32:34 +0545

    challenge completed

--- app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -66,6 +68,8 @@ public class SimpleFragment extends Fragment {
                 inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
 
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+
 // TODO: Set the radioGroup onCheckedChanged listener.
         radioGroup.setOnCheckedChangeListener(new
                                                       RadioGroup.OnCheckedChangeListener() {
@@ -88,6 +92,20 @@ public class SimpleFragment extends Fragment {
                                                               }
                                                           }
                                                       });
+
+        // Set the rating bar onCheckedChanged listener.
+        ratingBar.setOnRatingBarChangeListener
+                (new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar,
+                                                float rating, boolean fromUser) {
+                        // Get rating and show Toast with rating.
+                        String myRating = (getString(R.string.my_rating) +
+                                String.valueOf(ratingBar.getRating()));
+                        Toast.makeText(getContext(), myRating,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 // Return the View for the fragment's UI.
         return rootView;
